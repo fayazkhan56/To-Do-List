1dbfe417cea2fb67c9f01e25f14768d70567754d
fayazkhan56 <68395705+fayazkhan56@users.noreply.github.com>
2020-12-03 21:36:03 +0500

    second version

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const App = express();
-var items = ["Book reading","Have a cup of tea"];
+var items = ["Book reading", "Have a cup of tea"];
+var works = ['programming', "prayer"];
 App.set('view engine', 'ejs');  
 App.use(bodyParser.urlencoded({ extended: true }));
 App.use(express.static("public"));
@@ -11,13 +12,30 @@ App.get("/", function (req, res) {
     var options = { weekday: 'long', month: 'long', day: 'numeric' };
     var day = today.toLocaleDateString("en-US", options);
         var time = today.toLocaleTimeString("en-US");
-         res.render("list", { kindofday: day, kindoftime:time,newvalues:items });
+         res.render("list", { listTitle: day,newitems:items });
 } 
 );
 App.post("/", function (req, res) {
-    var item = req.body.newitem;
-    items.push(item);
-    res.redirect("/");
+    let item = req.body.newitem;
+    console.log(req.body.list);
+    if (req.body.list === "work-list") {
+       
+    works.push(item);
+    res.redirect("/work");
+    }
+    else {
+          items.push(item);
+    res.redirect("/"); 
+    }
+
+});
+
+App.get("/work", function (req, res) {
+    res.render("list", { listTitle: "work-list", newitems: works })
+});
+
+App.get("/about", function (req, res) {
+    res.render("about");
 })
 
 App.listen(3000, function () {
